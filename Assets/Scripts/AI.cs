916692d73e5e160bb9a550d5929653490197a2fd
ESTBLC <estblcmail@gmail.com>
2018-02-27 09:27:48 +0100

    Create mob with basic IA
    IA follow the player

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+    GameObject target;
+    NavMeshAgent agent = null;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        //target = GameObject.FindWithTag("Player");
+        agent.SetDestination(target.transform.position);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+            target = GameObject.FindWithTag("Player");
+        agent.SetDestination(target.transform.position);
+    }
+}
