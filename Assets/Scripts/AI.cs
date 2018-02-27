f8ac597828e63d53394b5e9b570efd1be40a6b7a
ESTBLC <estblcmail@gmail.com>
2018-02-27 19:21:01 +0100

    AI script clarification

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -5,20 +5,26 @@ using UnityEngine.AI;
 
 public class AI : MonoBehaviour {
 
-    GameObject target;
+    GameObject target = null;
     NavMeshAgent agent = null;
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-        //target = GameObject.FindWithTag("Player");
+        //target = GameObject.FindWithTag("Player"); Marche pas ???
         agent.SetDestination(target.transform.position);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Update Target
         if (target == null)
             target = GameObject.FindWithTag("Player");
         agent.SetDestination(target.transform.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.tag == "Ground")
     }
 }
