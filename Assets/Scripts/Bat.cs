195d7f8015abe3e55e4dfb1bf4dbaee767565005
Unknown <facqueur.xavier@sfr.fr>
2018-03-14 13:31:53 +0100

    C'est totalement incroyable, maintenant il y a des particules

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -5,16 +5,37 @@ using UnityEngine.Networking;
 
 public class Bat : Weapon {
 
-    // Use this for initialization
+    //Use this for initialization
     private bool alreadyHit = false;
+    [SerializeField]
+    private GameObject flare;
+
     public override void Fire(Player father)
     {
         GetComponent<Animation>().Play();
         base.Fire(father);
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        /*RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        // flare.GetComponent<ParticleSystem>().Play();
+        
+        Instantiate(flare, hit.point, Quaternion.LookRotation(transform.forward, transform.up));*/
+        Debug.Log("coucou");
+        ContactPoint contact = collision.contacts[0];
+        Instantiate(flare, contact.point, Quaternion.FromToRotation(Vector3.up, contact.normal));
+        
+
+    }
+    
     public override void OnTriggerStay(Collider collision)
     {
+
         if (!alreadyHit && GetComponent<Animation>().isPlaying && collision.tag == "Player")
         {
             base.OnTriggerStay(collision);
