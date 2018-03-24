cb50cf869b1f739c75430983561484f4d524ea1a
PierreL <pierre.lefort@epita.fr>
2018-03-24 21:07:28 +0100

    Rajout de petits scripts sympas

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    // Use this for initialization
+    public float damage = 10f;
+    public float range = 100f;
+
+    public ParticleSystem Shoot_particule;
+    public Camera cam;
+    
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        Shoot_particule.Play();
+        RaycastHit hit;        
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            // Ca fait rien pour l'instant
+
+            Debug.Log(hit.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/shrink.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrink : MonoBehaviour {
+
+    /*[SerializeField]
+    private Vector3 futurOrigin; // le centre du prochain shrink*/
+    [SerializeField]
+    private float targetScale;
+    [SerializeField]
+    private float shrinkSpeed; // the speed of the shrink
+
+    bool shrinking = true;
+    //private List<bool> shrinking = new List<bool> {false, false, false};
+
+	void Shrink() // the method to shrink
+    {
+        /*for (int i = 0; i < shrinking.Count; i++)
+            shrinking[i] = true;*/
+        shrinking = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (shrinking)
+        {
+            /*transform.root.localScale -= Vector3.one * Time.deltaTime * shrinkSpeed;*/
+            Vector3 Scale = new Vector3(Time.deltaTime * shrinkSpeed, 0, Time.deltaTime * shrinkSpeed);
+            transform.root.localScale -= Scale;
+            if (transform.root.localScale.x < targetScale || transform.root.localScale.z < targetScale)
+            {
+                shrinking = false;
+            }
+        }
+	}
+    /*
+    void shrinkaxes() // fonc to shrink each axes
+    {
+        float scaleaxe = transform.root.localScale[i];
+        scaleaxe -= Time.deltaTime * shrinkSpeed;
+        
+        }
+    }*/
+}
