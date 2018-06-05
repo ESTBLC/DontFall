e02dba68e7f273a73833274bb048de39bf5fad69
Unknown <pierre.lefort@epita.fr>
2018-06-05 05:34:37 +0200

    Try Network Timer

--- Assets/Menu/Load_Scene.cs
+++ Assets/Menu/Load_Scene.cs
@@ -7,7 +7,9 @@ public class Load_Scene : MonoBehaviour {
 
 	public void LoadScene(int lvl)
 	{
-        if(lvl != 0)
+        if (lvl == 0 || lvl == 1)
+            Cursor.visible = true;
+        else
             Cursor.visible = false;
 
 		SceneManager.LoadScene(lvl);
