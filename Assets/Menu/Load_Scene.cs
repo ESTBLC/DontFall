e6dcf8b783a7539db3571d46b133abc4feefb559
ESTBLC <estblcmail@gmail.com>
2018-06-06 02:37:51 +0200

    Network Dead

--- Assets/Menu/Load_Scene.cs
+++ Assets/Menu/Load_Scene.cs
@@ -8,7 +8,10 @@ public class Load_Scene : MonoBehaviour {
 	public void LoadScene(int lvl)
 	{
         if (lvl == 1 || lvl == 2)
+        {
             Cursor.visible = true;
+            PhotonNetwork.Disconnect();
+        }
         else
             Cursor.visible = false;
 
