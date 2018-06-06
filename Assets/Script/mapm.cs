290fb05cac97a569f2152a1bfe419168c3b9e622
Unknown <adrien942405@gmail.com>
2018-06-06 03:22:42 +0200

    grossemap en plus

---
+++ Assets/Script/mapm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapm : MonoBehaviour {
+
+    public GameObject menuobject;
+    bool isactive = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M))
+            {
+            inactivecam();
+        
+        }
+        if (isactive)
+        {
+            menuobject.SetActive(true);
+        }
+        else
+        {
+            menuobject.SetActive(false);
+        }
+	}
+    public void inactivecam()
+    {
+        isactive = !isactive;
+    }
+}
